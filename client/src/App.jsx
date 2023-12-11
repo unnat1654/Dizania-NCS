539eb8d9f564fe5327a7962776257a4cfc2f52fe
Unnat1654 <125393204+unnat1654@users.noreply.github.com>
2023-12-11 17:32:33 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,11 @@ import Signup from "./pages/auth/Signup";
 import Upload from "./pages/Upload";
 import Login from "./pages/auth/Login";
 import Home from "./pages/Home";
-<<<<<<< Updated upstream
 import Create from "./pages/profile/create/Create";
 import Show from "./pages/profile/show/Show";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-=======
 import Discover from "./pages/discover";
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -24,12 +21,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/upload" element={<Upload />} />
-<<<<<<< Updated upstream
         <Route path="/create_profile" element={<Create />} />
         <Route path="/show_profile" element={<Show user={""} />} />
-=======
         <Route path="/discover" element={<Discover />} />
->>>>>>> Stashed changes
       </Routes>
       <Footer />
     </div>
