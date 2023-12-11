3fae285b364c71a04dca3141e8c66ed2ce8b4c66
rohitPandey469 <rd8614196@gmail.com>
2023-12-11 19:25:26 +0530

    Changes profile model a little bit

--- Models/profileModel.js
+++ Models/profileModel.js
@@ -2,37 +2,88 @@ import mongoose from "mongoose";
 
 const profileSchema = new mongoose.Schema(
   {
+    googleId: String,
+
     username: {
       type: mongoose.ObjectId,
       ref: "Users",
       required: true,
     },
-    image: {
+
+    // ////////////////////
+    displayName: {
       type: String,
+      required: true,
     },
-    description: {
+    firstName: String,
+    lastName: String,
+    // image: {
+    //   type: String,
+    // },
+    profilePicture: {
       type: String,
-      maxlength: 1000,
+      default: "https://www.gravatar.com/avatar/?d=identicon",
     },
-    star: {
+    // description: {
+    //   type: String,
+    //   maxlength: 1000,
+    // },
+    bio: {
+      type: String,
+      maxlength: 50,
+      default:
+        "Passionate UI/UX Designer | 🎨 Creating Seamless Experiences |Problem Solver Extraordinaire | Let's Craft Digital Magic! ✨ #UIUXDesign",
+    },
+    // star: {
+    //   type: Number,
+    //   enum: [1, 2, 3, 4, 5],
+    // },
+    stars: {
       type: Number,
-      enum: [1, 2, 3, 4, 5],
+      enum: [0, 1, 2, 3, 4, 5],
+      default: 2,
     },
     rank: {
       type: Number,
+      default: 0,
+
     },
     competetion: {
       participated: {
         type: Number,
       },
-      streak: {
+      currStreak: {
         type: Number,
       },
       maxStreak: {
         type: Number,
       },
     },
-    tools: [{ type: mongoose.ObjectId, ref: "Tools" }],
+    // tools: [{ type: mongoose.ObjectId, ref: "Tools" }],
+    tools: [
+      {
+        name: {
+          type: String,
+          default: "figma",
+        },
+        slug: {
+          type: String,
+          default: "www.figma.com",
+        },
+      },
+    ],
+    most_used: [
+      {
+        name: {
+          type: String,
+          default: "figma",
+        },
+        slug: {
+          type: String,
+          default: "www.figma.com",
+        },
+      },
+    ],
     links: {
       instagram: {
         type: String,
@@ -50,9 +101,39 @@ const profileSchema = new mongoose.Schema(
         type: String,
       },
     },
+    connections: [
+      {
+        platform: {
+          type: String,
+          default: "linkedin",
+        },
+        url: {
+          type: String,
+          default: "www.linkedin.com",
+        },
+      },
+    ],
+    post: [
+      {
+        img: {
+          type: String,
+          // default: "./assets/post.png",
+        },
+        link: {
+          type: String,
+          default: "www.google.com",
+        },
+      },
+    ],
+    numCompetitions: {
+      type: Number,
+      default: 0,
+    },
     quote: {
       type: String,
-      maxlength: 200,
+      maxlength: 100,
+      default:
+        "Designing interfaces isn't just about pixels; it's about creating experiences that leave a lasting impression.",
     },
   },
   { timestamps: true }
